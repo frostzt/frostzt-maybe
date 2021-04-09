a1db279a17b263f760470f5c478fdbcbd2d85489
frostzt <aidenfrostbite@gmail.com>
2021-04-09 20:02:49 +0530

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,33 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+
+const Header = () => (
+  <header
+    style={{
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `black`,
+            textDecoration: `none`,
+          }}
+        >
+          frostzt maybe
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,54 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+// COMPONENTS
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+// STYLE
+import "../styles/index.scss"
+
+export default ({ data }) => {
+  console.log(data)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link className="post__title" to={node.fields.slug}>
+              <h3 className="heading-tertiary">
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </h3>
+            </Link>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            description
+            title
+            date
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
