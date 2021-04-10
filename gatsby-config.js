dde32527b73b338d63c46c8c27faf05edd4bfaee
frostzt <aidenfrostbite@gmail.com>
2021-04-10 11:34:46 +0530

    Implemented PWA and added icons and improved overall blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,8 +40,8 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-offline`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };
